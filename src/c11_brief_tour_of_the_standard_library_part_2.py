1815e5cb5600e6a36ea3cb3db6d2fcad36bdc70d
jashburn <jashburn8020@gmail.com>
2019-10-15 01:32:25 +0100

    Add tests for locale and Template

--- src/c11_brief_tour_of_the_standard_library_part_2.py
+++ src/c11_brief_tour_of_the_standard_library_part_2.py
@@ -7,6 +7,10 @@ import reprlib
 import pprint
 import contextlib
 import io
+import locale
+import datetime
+import pytest
+from string import Template
 
 
 def test_reprlib():
@@ -31,3 +35,62 @@ def test_pprint():
 [   ['black', 'cyan'],
     'white',
     ['green', 'red', [...]]]\n"""
+
+
+def localise_money(amount):
+    """Return localised monetary value based on the current locale"""
+    conv = locale.localeconv()
+    return locale.format_string("%s%.*f", (conv['currency_symbol'],
+                                           conv['frac_digits'], amount), grouping=True)
+
+
+def test_locale():
+    """The locale module accesses a database of culture specific data formats.
+    On Linux
+    - check supported locales: locale -a
+    - add locale: sudo locale-gen de_DE.UTF-8
+    - update locale settings: sudo update-locale"""
+    num = 12345678.90
+    date_time = datetime.datetime(2000, 1, 15, 13, 10, 5)
+
+    locale.setlocale(locale.LC_ALL, "zh_CN.UTF-8")
+    assert localise_money(num) == "￥12,345,678.90"
+    assert date_time.strftime("%c").strip() == "2000年01月15日 星期六 13时10分05秒"
+
+    locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
+    assert localise_money(num) == "€12.345.678,90"
+    assert date_time.strftime("%c").strip() == "Sa 15 Jan 2000 13:10:05"
+
+    locale.resetlocale()
+
+
+def test_template():
+    """The string module includes a Template class. The format uses placeholder names formed by $
+    with valid Python identifiers (alphanumeric characters and underscores).
+
+    The substitute() method raises a KeyError when a placeholder is not supplied in a dictionary or
+    a keyword argument. For mail-merge style applications, user supplied data may be incomplete and
+    the safe_substitute() method may be more appropriate."""
+
+    template = Template("${village}folk send $$10 to ${cause}")
+    replacements = {"village": "Nottingham", "cause": "the ditch fund"}
+    sentence = template.substitute(replacements)
+    assert sentence == "Nottinghamfolk send $10 to the ditch fund"
+
+    pytest.raises(KeyError, template.substitute, village="Nottingham")
+
+    incomplete_sentence = template.safe_substitute(village="Nottingham")
+    assert incomplete_sentence == "Nottinghamfolk send $10 to ${cause}"
+
+
+def test_template_customise():
+    """You can derive subclasses of Template to customize the delimiter character by overriding the
+    delimiter class attribute. You cannot change the delimiter after class creation."""
+
+    class PercentTemplate(Template):
+        """Subclass Template to specify a custom delimiter"""
+        delimiter = "%"
+
+    template = PercentTemplate("%{village}folk send $10 to %{cause} 50%% of the time")
+    sentence = template.substitute(village="Nottingham", cause="the ditch fund")
+    assert sentence == "Nottinghamfolk send $10 to the ditch fund 50% of the time"
