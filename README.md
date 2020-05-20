7b5d14f94295c042c0c850089beb5df96dbe9b7d
jashburn <jashburn8020@gmail.com>
2020-05-20 03:09:07 +0100

    Add notes and update examples for Chapter 3

--- README.md
+++ README.md
@@ -4,7 +4,84 @@ Examples from [The Python Tutorial](https://docs.python.org/3/tutorial/index.htm
 
 ## 3. An Informal Introduction to Python
 
+### Numbers
 
+- Integer numbers (e.g. 2, 4, 20) have type [`int`](https://docs.python.org/3/library/functions.html#int)
+- The ones with a fractional part (e.g. 5.0, 1.6) have type [`float`](https://docs.python.org/3/library/functions.html#float)
+- Division (`/`) always returns a `float`
+- To do floor division and get an integer result (discarding any fractional result) you can use the `//` operator
+  - to calculate the remainder you can use `%`
+- Use the `**` operator to calculate powers
+  - since `**` has higher precedence than `-`, `-3**2` will be interpreted as `-(3**2)` and thus result in `-9`.
+  - to avoid this and get `9`, you can use `(-3)**2`
+- Python supports other types of numbers, such as [`Decimal`](https://docs.python.org/3/library/decimal.html#decimal.Decimal) and [`Fraction`](https://docs.python.org/3/library/fractions.html#fractions.Fraction)
+- Python also has built-in support for [complex numbers](https://docs.python.org/3/library/stdtypes.html#typesnumeric)
+  - use the `j` or `J` suffix to indicate the imaginary part (e.g. `3+5j`)
+
+### Strings
+
+- See examples in [`src/c03_an_informal_introduction_to_python.py`](src/c03_an_informal_introduction_to_python.py)
+- Strings can be enclosed in single quotes (`'...'`) or double quotes (`"..."`)
+  - `\` can be used to escape quotes
+- If you don’t want characters prefaced by `\` to be interpreted as special characters, you can use **raw strings** by adding an `r` before the first quote
+  - see `test_raw_strings()`
+- String literals can span **multiple lines**
+  - one way is using triple-quotes: `"""..."""` or `'''...'''`
+  - end of lines are automatically included in the string
+    - possible to prevent this by adding a `\` at the end of the line
+  - see `test_multiline()`
+- Strings can be **concatenated** with the `+` operator, and **repeated** with `*`
+  - see `test_concat_repeat()`
+- Two or more string literals (i.e. the ones enclosed between quotes) next to each other are automatically concatenated
+  - see `test_concat_string_literal()`
+- Strings can be **indexed** (subscripted), with the first character having index 0
+  - there is no separate character type; a character is simply a string of size one
+  - indices may also be negative numbers, to start counting from the right
+  - see `test_string_index()`
+- **Slicing** is also supported
+  - allows you to obtain substring
+  - see `test_string_slice()`
+- Python strings cannot be changed - they are **immutable**
+  - assigning to an indexed position in the string results in an error
+  - see `test_immutable_string()`
+- The built-in function **`len()`** returns the length of a string
+  - `test_string_len()`
+- See also:
+  - [Text Sequence Type - str](https://docs.python.org/3/library/stdtypes.html#textseq)
+    - strings are examples of sequence types, and support the common operations supported by such types
+  - [String Methods](https://docs.python.org/3/library/stdtypes.html#string-methods)
+    - strings support a large number of methods for basic transformations and searching
+    - see `test_string_methods()`
+  - [Formatted string literals](https://docs.python.org/3/reference/lexical_analysis.html#f-strings)
+    - string literals that have embedded expressions
+    - see `test_formatted_string_literals()`
+  - [Format String Syntax](https://docs.python.org/3/library/string.html#formatstrings)
+    - information about string formatting with `str.format()`
+    - see `test_format_string_syntax()`
+  - [printf-style String Formatting](https://docs.python.org/3/library/stdtypes.html#old-string-formatting)
+    - the old formatting operations invoked when strings are the left operand of the `%` operator are described in more detail here
+
+### Lists
+
+- See examples in [`src/c03_an_informal_introduction_to_python.py`](src/c03_an_informal_introduction_to_python.py)
+- A list can be written as a list of comma-separated values (items) between square brackets
+- Lists might contain items of different types, but usually the items all have the same type
+- Lists can be **indexed** and **sliced**
+  - all slice operations return a new list containing the requested elements
+  - slice returns a shallow copy of the list
+  - see `test_list_index_slice()`
+- Lists also support operations like **concatenation** (`+`)
+  - see `test_list_concat()`
+- Lists are a **mutable** type, i.e. it is possible to change their content
+  - see `test_list_mutable()`
+- You can also add new items at the end of the list, by using the **`append()`** method
+  - see `test_list_append()`
+- **Assignment to slices** is also possible, and this can even change the size of the list or clear it entirely
+  - see `test_list_assign_to_slice()`
+- The built-in function **`len()`** also applies to lists
+  - see `test_list_len()`
+- It is possible to **nest** lists
+  - see `test_list_nest()`
 
 ## Source
 
--- src/c03_an_informal_introduction_to_python.py
+++ src/c03_an_informal_introduction_to_python.py
@@ -1,128 +1,162 @@
-#!/usr/bin/pytest-3
 """
 An Informal Introduction to Python
 https://docs.python.org/3/tutorial/introduction.html
 """
-
-# Strings
-
-
-def test_raw_strings():
-    """Raw Strings"""
-    path = r"c:\some\name"
-    assert path == "c:\\some\\name"
+import pytest
 
 
-def test_multiline():
-    """Multiline strings"""
-    multiline = """\
+class TestStrings:
+    def test_raw_strings(self):
+        """Raw Strings"""
+        path = r"c:\some\name"
+        assert path == "c:\\some\\name"
+
+    def test_multiline(self):
+        """Multiline strings"""
+        multiline = """\
 one
 two"""
-    assert multiline == "one\ntwo"
+        assert multiline == "one\ntwo"
+
+    def test_concat_repeat(self):
+        """String concatenation and repetition"""
+        some_str = 2 * "pa" + "dum"
+        assert some_str == "papadum"
+
+    def test_concat_string_literal(self):
+        """Concatenating literal strings"""
+        some_str = "Breaking a long " "string"
+        assert some_str == "Breaking a long string"
+
+    def test_string_index(self):
+        """String indices"""
+        some_str = "python"
+        assert some_str[0] == "p"
+        assert some_str[5] == "n"
+        assert some_str[5] == some_str[-1]
+
+    def test_string_slice(self):
+        """String slicing"""
+        some_str = "python"
+        assert some_str[:2] == "py"
+        assert some_str[2:4] == "th"
+        assert some_str[4:] == "on"
+        assert some_str[-2:] == some_str[4:]
+        assert some_str[4:100] == "on"
+
+    def test_immutable_string(self):
+        """Assigning to an indexed position results in an error"""
+        word = "python"
+        with pytest.raises(TypeError):
+            word[0] = "J"
+
+        # If you need a different string, create a new one
+        assert "Jython" == "J" + word[1:]
+
+    def test_string_len(self):
+        """String length"""
+        some_str = "python"
+        assert len(some_str) == 6
+
+    def test_string_methods(self):
+        """
+        String methods
+        https://docs.python.org/3/library/stdtypes.html#string-methods
+        """
+        some_str = "python"
+        assert some_str.endswith("o", 3, 5)
+        assert some_str.endswith(("m", "n"))
+        assert some_str.rjust(10, "*") == "****python"
+
+    def test_formatted_string_literals(self):
+        """
+        Formatted string literals
+        https://docs.python.org/3/reference/lexical_analysis.html#f-strings
+        """
+        name = "Fred"
+        assert f"He said his name is {name!r}" == "He said his name is 'Fred'"
+
+        width = 7
+        precision = 4
+        value = 12.34567
+        assert f"result:{value:{width}.{precision}}" == "result:  12.35"
+
+    def test_format_string_syntax(self):
+        """
+        Format string syntax
+        https://docs.python.org/3/library/string.html#formatstrings
+        """
+        assert "{1}, {0}, {2}".format("a", "b", "c") == "b, a, c"
+        assert "{}, {}, {}".format("a", "b", "c") == "a, b, c"
+        assert (
+            "Coordinates: {latitude}, {longitude}".format(
+                latitude="37N", longitude="15W"
+            )
+            == "Coordinates: 37N, 15W"
+        )
+
+        # accessing argument's attributes
+        complex_num = 3 - 5j
+        assert (
+            "{0}: real {0.real}, imaginary {0.imag}".format(complex_num)
+            == "(3-5j): real 3.0, imaginary -5.0"
+        )
 
 
-def test_concat_repeat():
-    """String concatenation and repetition"""
-    some_str = 3 * "un" + "nium"
-    assert some_str == "unununnium"
+class TestLists:
+    def test_list_index_slice(self):
+        """List indexing and slicing"""
+        squares = [1, 4, 9, 16, 25]
+        assert squares[4] == 25
+        assert squares[2] == squares[-3]
+        assert squares[2:] == [9, 16, 25]  # new list
 
+        squares[1:3] = [36, 49]
+        assert squares == [1, 36, 49, 16, 25]
+        squares[2:4] = []
+        assert squares == [1, 36, 25]
 
-def test_concat_string_literal():
-    """Concatenating literal strings"""
-    some_str = ("Breaking a long "
-                "string")
-    assert some_str == "Breaking a long string"
+    def test_list_concat(self):
+        """List concatenation"""
+        squares = [1, 4, 9]
+        squares = squares + [16, 25]
+        assert squares == [1, 4, 9, 16, 25]
 
+    def test_list_mutable(self):
+        """Lists are mutable"""
+        cubes = [1, 8, 27, 65]
+        cubes[3] = 64
+        assert cubes == [1, 8, 27, 64]
 
-def test_string_index():
-    """String indices"""
-    some_str = "python"
-    assert some_str[0] == "p"
-    assert some_str[5] == "n"
-    assert some_str[5] == some_str[-1]
+    def test_list_append(self):
+        """Appending to a list"""
+        squares = [1, 4, 9]
+        squares.append(4 ** 2)
+        assert squares == [1, 4, 9, 16]
 
+    def test_list_assign_to_slice(self):
+        """Assignment to slices"""
+        letters = ["a", "b", "c", "d", "e"]
+        letters[1:3] = ["B", "C"]
+        assert letters == ["a", "B", "C", "d", "e"]
 
-def test_string_slice():
-    """String slicing"""
-    some_str = "python"
-    assert some_str[:2] == "py"
-    assert some_str[2:4] == "th"
-    assert some_str[4:] == "on"
-    assert some_str[-2:] == some_str[4:]
-    assert some_str[4:100] == "on"
+        # Remove some items
+        letters[3:] = []
+        assert letters == ["a", "B", "C"]
 
+        # Clear the list
+        letters[:] = []
+        assert letters == []
 
-def test_string_len():
-    """String length"""
-    some_str = "python"
-    assert len(some_str) == 6
+    def test_list_len(self):
+        """List length"""
+        assert len(["a", "b", "c", "d", "e"]) == 5
 
+    def test_list_nest(self):
+        """Nested list"""
+        letters = ["a", "b", "c"]
+        numbers = [1, 2]
+        letters_and_numbers = [letters, numbers]
 
-def test_string_methods():
-    """
-    String methods
-    https://docs.python.org/3/library/stdtypes.html#string-methods
-    """
-    some_str = "python"
-    assert some_str.endswith("o", 3, 5)
-    assert some_str.endswith(('m', 'n'))
-    assert some_str.rjust(10, "*") == "****python"
-
-
-def test_formatted_string_literals():
-    """
-    Formatted string literals
-    https://docs.python.org/3/reference/lexical_analysis.html#f-strings
-    """
-    name = "Fred"
-    assert f"He said his name is {name!r}" == "He said his name is 'Fred'"
-
-    width = 7
-    precision = 4
-    value = 12.34567
-    assert f"result:{value:{width}.{precision}}" == "result:  12.35"
-
-
-def test_format_string_syntax():
-    """
-    Format string syntax
-    https://docs.python.org/3/library/string.html#formatstrings
-    """
-    assert "{1}, {0}, {2}".format("a", "b", "c") == "b, a, c"
-    assert "{}, {}, {}".format("a", "b", "c") == "a, b, c"
-    assert "Coordinates: {latitude}, {longitude}".format(
-        latitude="37N", longitude="15W") == "Coordinates: 37N, 15W"
-
-    # accessing argument's attributes
-    complex_num = 3 - 5j
-    assert "{0}: real {0.real}, imaginary {0.imag}".format(
-        complex_num) == "(3-5j): real 3.0, imaginary -5.0"
-
-# Lists
-
-
-def test_list_index_slice():
-    """List indexing and slicing"""
-    squares = [1, 4, 9, 16, 25]
-    assert squares[4] == 25
-    assert squares[2] == squares[-3]
-    assert squares[2:] == [9, 16, 25]  # new list
-
-    squares[1:3] = [36, 49]
-    assert squares == [1, 36, 49, 16, 25]
-    squares[2:4] = []
-    assert squares == [1, 36, 25]
-
-
-def test_list_concat():
-    """List concatentation"""
-    squares = [1, 4, 9]
-    squares = squares + [16, 25]
-    assert squares == [1, 4, 9, 16, 25]
-
-
-def test_list_append():
-    """Appending to a list"""
-    squares = [1, 4, 9]
-    squares.append(4 ** 2)
-    assert squares == [1, 4, 9, 16]
+        assert letters_and_numbers == [["a", "b", "c"], [1, 2]]
+        assert letters_and_numbers[1][0] == 1
